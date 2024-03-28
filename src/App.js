b179f190023dc2ca8772a1979bab59a3acbee07e
Sunny <sunnysagar620@gmail.com>
2024-03-28 18:46:07 +0530

    "intial commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+//  installing dependencies and chat-engine
+import { ChatEngine } from "react-chat-engine";
+
+import  ChatFeed  from './components/ChatFeed';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID= "4b6cc744-03fa-452b-9360-05ad00e9773b"
+            userName="zukar09"
+            userSecret="2789"
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} />}
+        />
+
+    );
 }
 
 export default App;
